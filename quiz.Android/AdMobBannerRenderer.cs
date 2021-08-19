22d0a9a419d5c1da1cd7c4a1aa67e4de1bb2c85a
maromilovers <76511730+maromilovers@users.noreply.github.com>
2021-08-19 17:14:10 +0900

    Add files via upload

---
+++ quiz.Android/AdMobBannerRenderer.cs
@@ -0,0 +1,35 @@
+﻿using quiz;
+using AddAdMobBannerSample.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+#pragma warning disable CS0612 // 型またはメンバーが旧型式です
+[assembly: ExportRenderer(typeof(AdMobBanner), typeof(AdMobBannerRenderer))]
+#pragma warning restore CS0612 // 型またはメンバーが旧型式です
+namespace AddAdMobBannerSample.Droid.Renderers
+{
+    [System.Obsolete]
+    public class AdMobBannerRenderer : ViewRenderer<AdMobBanner, Android.Gms.Ads.AdView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<AdMobBanner> e)
+        {
+            //テスト用
+            //const string adUnitID = "ca-app-pub-3940256099942544/6300978111";
+            const string adUnitID = "ca-app-pub-4439948160128694/2236451104";
+
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var adMobBanner = new Android.Gms.Ads.AdView(Forms.Context);
+                adMobBanner.AdSize = Android.Gms.Ads.AdSize.Banner;
+                adMobBanner.AdUnitId = adUnitID;
+
+                var requestbuilder = new Android.Gms.Ads.AdRequest.Builder();
+                adMobBanner.LoadAd(requestbuilder.Build());
+
+                SetNativeControl(adMobBanner);
+            }
+        }
+    }
+}
---
+++ quiz.Android/SoundEffect.cs
@@ -0,0 +1,53 @@
+﻿using Android.Media;
+using quiz;
+using quiz.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Sound01.Droid.SoundEffect))]
+namespace Sound01.Droid
+{
+    public class SoundEffect : ISoundEffect
+    {
+        SoundPool soundPool;
+        int hitSoundId;
+        int correctSoundId;
+        int incorrectSoundId;
+        int resultSoundId;
+
+        public SoundEffect()
+        {
+            int SOUND_POOL_MAX = 6;
+
+            AudioAttributes attr = new AudioAttributes.Builder()
+                .SetUsage(AudioUsageKind.Media)
+                .SetContentType(AudioContentType.Music)
+                .Build();
+            soundPool = new SoundPool.Builder()
+               .SetAudioAttributes(attr)
+               .SetMaxStreams(SOUND_POOL_MAX)
+               .Build();
+            hitSoundId = soundPool.Load(Android.App.Application.Context, Resource.Raw.touch, 1);
+            correctSoundId = soundPool.Load(Android.App.Application.Context, Resource.Raw.maru, 1);
+            incorrectSoundId = soundPool.Load(Android.App.Application.Context, Resource.Raw.batu, 1);
+            resultSoundId = soundPool.Load(Android.App.Application.Context, Resource.Raw.result, 1);
+        }
+
+        public void HitSound()
+        {
+            soundPool.Play(hitSoundId, 1.0F, 1.0F, 0, 0, 1.0F);
+        }
+
+        public void CorrectSound()
+        {
+            soundPool.Play(correctSoundId, 1.0F, 1.0F, 0, 0, 1.0F);
+        }
+
+        public void IncorrectSound()
+        {
+            soundPool.Play(incorrectSoundId, 1.0F, 1.0F, 0, 0, 1.0F);
+        }
+        public void ResultSound()
+        {
+            soundPool.Play(resultSoundId, 1.0F, 1.0F, 0, 0, 1.0F);
+        }
+    }
+}
